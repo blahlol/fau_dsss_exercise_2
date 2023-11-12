1fef210e3439da40110b31826a3b1c0a6f9c1bf3
blahlol <karanmahesh1905@gmail.com>
2023-11-12 17:02:50 +0530

    updated setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='math_quiz_game',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A math quiz game with randomly generated problems',
+    url='https://github.com/blahlol/fau_dsss_exercise_2',
+)
